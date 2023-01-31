19ad8195d50210f4b52e5a02bf6d4a54517aafb6
ts-chongyuan.zhong <ts-chongyuan.zhong@rakuten.com>
2023-01-31 18:01:06 +0800

    Initial Commit

--- src/main/java/JsonJava/JsonToJava.java
+++ src/main/java/JsonJava/JsonToJava.java
@@ -1,2 +1,40 @@
-package JsonJava;public class JsonToJava {
+package JsonJava;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+import java.io.IOException;
+import java.sql.*;
+import java.util.ArrayList;
+
+public class JsonToJava {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException, IOException {
+        ArrayList<CumtomerDetails> cumtomerDetailsArrayList =new ArrayList();
+
+
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection conn=null;
+        conn=DriverManager.getConnection("jdbc:mysql://localhost:3306/Business","root","admin123");
+//        Object of statement class will help us to execute queries
+        Statement statement = conn.createStatement();
+//        statement.execute("use Business");
+        ResultSet resultSet = statement.executeQuery("select * from CustomerInfo where Location ='Asia' and PurchasedDate = curdate() ;");
+
+//        int i=1;
+        while(resultSet.next()){
+            CumtomerDetails c = new CumtomerDetails();
+            c.setCourseName(resultSet.getString(1));
+            c.setPurchaseDate(resultSet.getString(2));
+            c.setAmount(resultSet.getInt(3));
+            c.setLocation(resultSet.getString(4));
+            cumtomerDetailsArrayList.add(c);
+        }
+
+        ObjectMapper o=new ObjectMapper();
+        o.writeValue(new File("sdet-essentials.json"),cumtomerDetailsArrayList);
+
+
+        conn.close();
+
+    }
 }
